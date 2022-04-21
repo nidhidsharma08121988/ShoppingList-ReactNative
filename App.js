ed78902c80c42ef58dc6ebf835f807a2a4fe9845
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-04-21 12:25:24 +0530

    styles: Add style to the title of app

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Shoppping List</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Shoppping List</Text>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: 'white',
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
 export default App;
