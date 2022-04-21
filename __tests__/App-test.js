2457a1971b5fdfc861ec71f537d1691fac69523b
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-04-21 11:40:07 +0530

    Test(failed): Add failed test to display heading Shopping list

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,14 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react-native';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+it('Displays heading Shopping List', () => {
+  const {queryByText} = render(<App />);
+  expect(queryByText('Shopping list')).toBe(true);
 });
