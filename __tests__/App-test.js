9fdcdecabd265f0c98df28afe8a687d655875460
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-04-22 17:48:53 +0530

    test(failed): Add failed test: input-box and add icon must be displayed

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -8,37 +8,55 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import {fireEvent, render, waitFor} from '@testing-library/react-native';
 
-it('displays heading Shopping List', () => {
-  const {queryByText} = render(<App />);
-  const title = queryByText('Shopping List');
-  expect(title).toBeTruthy();
-});
-
-it('displays shopping list items', () => {
-  const {queryByText} = render(<App />);
-  const listOfItems = [
-    'Milk',
-    'Cookies',
-    'Notebook',
-    'Pencils',
-    'DishWash liquid',
-  ];
-
-  listOfItems.map(itemText => {
-    expect(queryByText(itemText)).toBeTruthy();
+describe('App', () => {
+  describe('Header', () => {
+    it('displays heading Shopping List', () => {
+      const {queryByText} = render(<App />);
+      const title = queryByText('Shopping List');
+      expect(title).toBeTruthy();
+    });
   });
-});
 
-it('displays delete icon for each item in shopping list', () => {
-  const {queryAllByTestId} = render(<App />);
-  expect(queryAllByTestId('remove-icon').length).toBe(5);
-});
+  describe('List of items', () => {
+    it('must be displayed', () => {
+      const {queryByText} = render(<App />);
+      const listOfItems = [
+        'Milk',
+        'Cookies',
+        'Notebook',
+        'Pencils',
+        'DishWash liquid',
+      ];
 
-it('when user clicks delete, item is removed', async () => {
-  const {queryByText, getAllByTestId} = render(<App />);
-  const milkDelBtn = getAllByTestId('remove-icon')[0];
-  fireEvent.press(milkDelBtn);
-  await waitFor(() => {
-    expect(queryByText('Milk')).toBeFalsy();
+      listOfItems.map(itemText => {
+        expect(queryByText(itemText)).toBeTruthy();
+      });
+    });
+  });
+  describe('Delete Item', () => {
+    it('displays delete icon', () => {
+      const {queryAllByTestId} = render(<App />);
+      expect(queryAllByTestId('remove-icon').length).toBe(5);
+    });
+    it('when user clicks delete, item is removed', async () => {
+      const {queryByText, getAllByTestId} = render(<App />);
+      const milkDelBtn = getAllByTestId('remove-icon')[0];
+      fireEvent.press(milkDelBtn);
+      await waitFor(() => {
+        expect(queryByText('Milk')).toBeFalsy();
+      });
+    });
+  });
+
+  describe('Add item', () => {
+    it('displays add icon', () => {
+      const {queryByTestId} = render(<App />);
+      expect(queryByTestId('add-icon')).toBeTruthy();
+    });
+    it('displays input box for text', () => {
+      const {queryByTestId} = render(<App />);
+      expect(queryByTestId('user-input')).toBeTruthy();
+    });
+    it('when user clicks add icon, the item must be added to list', () => {});
   });
 });
